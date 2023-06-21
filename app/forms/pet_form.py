8207f583b4aaa01ef71b8a2a4e2179d4c95cc4ab
James Hoang <hoangjames23@gmail.com>
2023-06-21 15:51:30 -0700

    Finished API route

--- app/forms/pet_form.py
+++ app/forms/pet_form.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, Email, Length, ValidationError
 from app.models.pet import Pet
 
 
 class PetForm(FlaskForm):
-    name = StringField(validators=[Length(min=1, max=26, message="Tag name must be 1 to 26 characters")])
+    name = StringField('name',validators=[Length(min=1, max=26, message="Tag name must be 1 to 26 characters")])
+    happiness = IntegerField('happiness')
     submit = SubmitField('Post')
