5ed3ab72b44260f7b6e93e51b0b573e78e8e61fb
James Hoang <hoangjames23@gmail.com>
2023-06-22 17:47:34 -0700

    still wokring on it

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,4 +1,5 @@
 import "./landing.css"
+import sketchbook from "../../assets/sketchbook.png"
 
 const LandingPage = () => {
     const avatar = "https://img.wattpad.com/286ea0e7dc93d535f9e04dae613223d3d4ddfac9/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f65454337674630684d594f5276773d3d2d3637353137303937352e313537356430373466313763313438623338393034323838373636362e6a7067?s=fit&w=720&h=720"
@@ -27,15 +28,44 @@ const LandingPage = () => {
                     </div>
                     <div className="virtual-pet-container">
                         <div className="virtual-pet-box">
-                            <img src={pet} alt="pet" />
+                            <img src={pet} alt="pet" className="pet" />
+                            <div className="pet-text">🔴BROADCASTING LIVE...</div>
                         </div>
-                        <div className="calendar">
-
+                        <div className="chat-wrapper">
+                            <div className="chat-header">Chat</div>
+                            <div className="chat-container">
+                                <div className="message">Froggy found a coin.</div>
+                                <div className="message">Froggy found a coin.</div>
+                                <div className="message">Froggy found a coin.</div>
+                                <div className="message">Froggy found a coin.</div>
+                                <div className="message">Froggy found a coin.</div>
+                                <div className="message">Froggy found a coin.</div>
+                                <div className="message">Froggy found a coin.</div>
+                                <div className="message">Froggy found a coin.</div>
+                                <div className="message">Froggy found a coin.</div>
+                                <div className="message">Froggy found a coin.</div>
+                                <div className="message">Froggy found a coin.</div>
+                            </div>
+                            <form className="send-chat-form">
+                                <input type="text" placeholder="Streamer has restricted chat..." disabled={true} />
+                                <button type="submit" disabled={true}>Send</button>
+                            </form>
                         </div>
                     </div>
+                    <div>
+                        <div className="pet-info-container">
+                            <div className="pet-info-wrapper">
+                                <div className="about-me-pet-header"> About Me</div>
+                                <div className="about-me">Hello my name is Froggy. I'm here to help my owner, Miranda!</div>
+                            </div>
+                            <div className="journal-buttons">
+                                <img src={sketchbook} alt="journal" className="journal-button" />
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-                <div className="land-container-2">
-                </div>
+
             </div>
         </div>
     )
