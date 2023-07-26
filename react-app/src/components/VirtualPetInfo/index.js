ce1b56fc7f739c1d9ef96e8e89f38df991292b59
James Hoang <hoangjames23@gmail.com>
2023-07-25 17:46:04 -0700

    pet info display

--- react-app/src/components/VirtualPetInfo/index.js
+++ react-app/src/components/VirtualPetInfo/index.js
@@ -1,11 +1,24 @@
 
+import "./virtualPetInfo.css"
+import virtualPet from "../../assets/unga.png"
+
 const VirtualPetInfo = ({ pet }) => {
     console.log(pet)
     return (
         <>
             <div className="pet-info-wrapper">
+                <img src={pet.bannerImgUrl} alt="banner" className="pet-banner-img" />
                 <div className="pet-info-container">
-                    lalal
+                    <div className="pet-pic">
+                        <img src={virtualPet} alt="pet" className="pet-info-img" />
+
+                    </div>
+                    <div className="pet-info-info">
+                        <h1>{pet.name}</h1>
+                        <p>❤️ {pet.happiness}/100</p>
+                        <p>🎟️ x{pet.ticket}</p>
+                        <p>Date of Birth: {pet.createdAt}</p>
+                    </div>
                 </div>
             </div>
         </>
